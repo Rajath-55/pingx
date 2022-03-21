61b7350a5f2f6c4d96e28bd8f7fc98eb23925e03
Shameek Kumar Baranwal <shameekbaranwal@gmail.com>
2022-03-21 20:51:07 +0530

    Initialized web client and added basic event handlers to server.

--- server/index.js
+++ server/index.js
@@ -1,20 +1,37 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import http from 'http';
-import * as socketio from 'socket.io';
+// import { v4 as uuidv4 } from 'uuid';
+import { Server } from 'socket.io';
+let users = 0;
 
+// env variables from .env file
 dotenv.config();
-
-const app = express();
-const server = http.createServer(app);
-// const io = socketio.createServer(server);
-
 const port = process.env.PORT || 5500;
 
-app.get('/', (req, res) => {
-	const loc = req.headers.host;
-	console.log(`Joined from:\n${loc}`);
-	res.send('🤡👍');
+// initializing server and socket.io
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// setting the static folder
+app.use(express.static('web-client/build/'));
+
+// setting the socket handlers
+io.on('connection', socket => {
+	console.log('a new user has connected.');
+
+	socket.broadcast.emit('new user', { usersInRoom: users++ });
+
+	socket.on('disconnect', () => {
+		socket.emit('disconnected', { usersInRoom: users-- });
+		console.log('a user has disconnected.');
+	});
+
+	socket.on('new message', data => {
+		socket.broadcast.emit('new message', data);
+		console.log(data);
+	});
 });
 
 server.listen(port, () => console.log(`Listening on PORT=${port}`));
