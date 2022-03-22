075f4bdd81669ea46b1f0df88703cea4c3c81962
Shameek Kumar Baranwal <shameekbaranwal@gmail.com>
2022-03-23 00:41:23 +0530

    qUIrky

--- server/web-client/src/components/Button.jsx
+++ server/web-client/src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Button({ onClick, children }) {
+export default function Button({ onClick, children, className }) {
 	return (
 		<button
-			className='transition-all mt-4 md:mt-6 mx-2 sm:mx-4 bg-blue-500 rounded-lg py-3 px-1 text-lg hover:bg-opacity-40 hover:bg-blue-600 focus:outline-none focus:ring-4 hover:shadow-md focus:shadow-sm sm:max-w-sm'
+			className={`transition-all mx-2 sm:mx-4 bg-blue-500 rounded-lg py-3 px-1 text-lg hover:bg-opacity-40 hover:bg-blue-600 focus:outline-none focus:ring-4 hover:shadow-md focus:shadow-sm sm:max-w-sm ${className}`}
 			onClick={onClick}
 		>
 			{children}
